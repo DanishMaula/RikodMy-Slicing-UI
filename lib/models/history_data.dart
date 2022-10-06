ece53ee199e7c5dc8002ce8e86d1ac1f40d1d29a
Danish <danishmaulahasbi@gmail.com>
2022-10-06 09:26:34 +0700

    add data for history

---
+++ lib/models/history_data.dart
@@ -0,0 +1,60 @@
+class HistoryData {
+  String code;
+  String date;
+  String name;
+  String price;
+  String clock;
+  String image;
+
+  HistoryData({
+    required this.code,
+    required this.date,
+    required this.name,
+    required this.price,
+    required this.clock,
+    required this.image
+  });
+}
+
+List<HistoryData> HistoryDataList = [
+  HistoryData(
+    code: '#TP1230000724',
+    date: '07.10.2021',
+    name: 'Raeihan Alvaro',
+    price: 'RM 1075.25',
+    clock: '16:17:18', 
+    image: 'https://sinarjambi.com/wp-content/uploads/2022/06/IMG_20220607_140124_125-scaled.jpg',
+      ),
+  HistoryData(
+    code: '#TP1230000724',
+    date: '07.10.2021',
+    name: 'Salman Khan',
+    price: 'RM 4524.25',
+    clock: '16:17:18', 
+    image: 'https://sinarjambi.com/wp-content/uploads/2022/06/IMG_20220607_140124_125-scaled.jpg',
+      ),
+  HistoryData(
+    code: '#TP1230000724',
+    date: '07.10.2021',
+    name: 'Shahrul Nizam',
+    price: 'RM 2263.25',
+    clock: '16:17:18', 
+    image: 'https://sinarjambi.com/wp-content/uploads/2022/06/IMG_20220607_140124_125-scaled.jpg',
+      ),
+  HistoryData(
+    code: '#TP1230000724',
+    date: '07.10.2021',
+    name: 'Hritik Roshan',
+    price: 'RM 31341232.25',
+    clock: '16:17:18', 
+    image: 'https://sinarjambi.com/wp-content/uploads/2022/06/IMG_20220607_140124_125-scaled.jpg',
+      ),
+    HistoryData(
+    code: '#TP1230000724',
+    date: '07.10.2021',
+    name: 'Aamir Khan',
+    price: 'RM 12331223.25',
+    clock: '16:17:18', 
+    image: 'https://sinarjambi.com/wp-content/uploads/2022/06/IMG_20220607_140124_125-scaled.jpg',
+      ),
+];
